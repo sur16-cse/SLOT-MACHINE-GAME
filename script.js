04d2a7c885d90bea0b6009a55368367a5071fd3c
surbhi kumari <surbhikumaridav@gmail.com>
2021-09-03 14:40:02 +0530

    Test Your Destiny

---
+++ script.js
@@ -0,0 +1,46 @@
+let a=document.getElementsByTagName("p")[0];
+let b=document.getElementsByTagName("p")[1];
+let c=document.getElementsByTagName("p")[2];
+
+
+let values=[
+    '😍', '🤣' ,'😒', '😢', '🤩', '🙄', '🤐' ,'🤗', '😣', '🤨', '😂', '😮', '🥰', '😏', '😋', '😑','🥱','🥵','😡'
+    ,'😈','🥳'
+];
+
+var inter;
+
+function getRandomValue(){
+    return values[Math.floor(Math.random()*values.length)];
+}
+function add(){inter=setInterval(() => {
+    a.innerText=getRandomValue();
+    b.innerText=getRandomValue();
+    c.innerText=getRandomValue();
+}, 200);
+}
+
+
+
+remove=()=> {
+   setTimeout(() => {
+       clearInterval(inter);
+    if(a===b===c){
+        document.getElementById("display").innerHTML="YOU WIN" 
+    }
+    else
+    {
+        document.getElementById("display").innerHTML="YOU LOSE"
+    }
+   }, 1000);   
+}
+
+
+
+
+var start=document.getElementById("start");
+start.addEventListener('click',add);
+
+
+var stop=document.getElementById("stop");
+stop.addEventListener('click',remove);
